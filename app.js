ec5daa8527d29a621c05a7a57c9794c8cad7b7ef
hooty868 <hooty868@gmail.com>
2021-01-21 21:08:10 +0800

    -m feat: add main .hbs and modiy index.hbs for currency caculate and add post for new and delete event

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
+const catchCurrency = require('./public/js/catchPrice')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/stoke', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
 
-const exphbs = require('express-handlebars')
+const db = mongoose.connection
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
 
 db.on('error', () => {
   console.log('mongodb error!')
@@ -18,13 +23,63 @@ db.once('open', () => {
 
 const Todo = require('./models/todo')
 app.get('/', (req, res) => {
-  Todo.find()
-    .lean()
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.error(error))
+  async function doThings() {
+    try {
+      const currencyFinal = await catchCurrency()
+      Todo.find()
+        .lean()
+        .then(todos => {
+          let final = []
+          let total = 0
+          final = todos.map(todo => {
+            if (todo.name === '美金 (USD)') {
+              const obj = {
+                ...todo,
+                ...currencyFinal[0]
+              }
+              return obj
+            } else if (todo.name === '人民幣 (CNY)') {
+              const obj = {
+                ...todo,
+                ...currencyFinal[1]
+              }
+              return obj
+            }
+          })
+          final = final.map(currency => {
+            let obj = {
+              ...currency,
+              profit: (currency.buyPrice / currency.exchange) * (currency.spot_ask - currency.exchange)
+            }
+            total += obj.profit
+            return obj
+          })
+          total = Math.round(total)
+          res.render('index', { final, total })
+          // console.log(final)
+        })
+        .catch(error => console.error(error))
+    } catch (error) {
+      console.log(error)
+    }
+  } doThings()
 })
 
-// 設定 port 3000
+app.post('/currency/new', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name: name[0], exchange: name[1], buyPrice: name[2], date: new Date() })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.post('/currency/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
