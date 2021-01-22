2876ba6993a36995b9c71bb5f019b3834da5f976
hooty868 <hooty868@gmail.com>
2021-01-23 02:00:54 +0800

    modify mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/stoke', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/stoke', { useNewUrlParser: true, useUnifiedTopology: true })// somthing must be modify
 const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
